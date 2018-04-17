67adf91e4d17ced5861ca4c59ada2fb25c601cf3
leduchuy-1420195 <Duchuy.le2212@gmail.com>
2018-04-17 19:22:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/minhthanh/restaurantorderapps/ListFoodsToOrder.java
@@ -0,0 +1,13 @@
+package com.example.minhthanh.restaurantorderapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListFoodsToOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_foods_to_order);
+    }
+}
